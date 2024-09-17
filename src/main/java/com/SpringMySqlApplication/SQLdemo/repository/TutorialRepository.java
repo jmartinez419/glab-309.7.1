38c3257c670f9b46cabb01fdc6bd81d1fad54f32
jmartinez419 <justynmartinez419@gmail.com>
2024-09-17 11:09:37 -0400

    modified for glab 310.1.1

--- src/main/java/com/SpringMySqlApplication/SQLdemo/repository/TutorialRepository.java
+++ src/main/java/com/SpringMySqlApplication/SQLdemo/repository/TutorialRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Repository
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(path = "learningtutorials")
+//@Repository
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
         // Tutorial → Entity type and Long --> primary key
 {
